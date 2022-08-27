3c4850ea5c2eb4118e2c1939be61e58503f529d1
SHADOWKG <112260203+SHADOWKG@users.noreply.github.com>
2022-08-27 21:56:15 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,30 +1,22 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wxd8b296275f1396e1",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "d328083b9a70198cdf5219c943e8aae7",
     // 模板消息id
-    templateId: "",
+    templateId: "yC1Y5TrLki_uwmhvN5Ebf_bzsH1Yj3reu1F9oQvWkF4",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+    user: ["China20021008"],
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "北京",
     // 所在城市
-    city: "惠州",
-    // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
-    birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
-      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
-    ],
+    city: "北京",
+   
     // 在一起的日子，格式同上
-    loveDate: "2015-05-01",
-    // 结婚纪念日
-    marryDate: "2020-01-04"
-    }
-
+    loveDate: "2022-05-20",
 // {{date.DATA}}  
 // 城市：{{city.DATA}}  
 // 天气：{{weather.DATA}}  
